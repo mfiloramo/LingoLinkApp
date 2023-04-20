85292257e0a69c215fe60ee1034ca87f3da817a2
mfiloramo <mlfiloramo@gmail.com>
2023-04-20 11:20:29 -0400

    Changed env var syntax in environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment: any = {
-  production: '#{PRODUCTION}',
+  production: '${PRODUCTION}',
   redirectURI: location.origin,
-  apiBaseUrl: '#{API_BASE_URL}',
-  azureTenantURL: '#{AZURE_TENANT_URL}',
-  azureClientID: '#{AZURE_CLIENT_ID}',
-  azureAPIClientID: '#{AZURE_API_CLIENT_ID}',
-  appConfigEndpoint: '#{APP_CONFIG_ENDPOINT}',
+  apiBaseUrl: '${API_BASE_URL}',
+  azureTenantURL: '${AZURE_TENANT_URL}',
+  azureClientID: '${AZURE_CLIENT_ID}',
+  azureAPIClientID: '${AZURE_API_CLIENT_ID}',
+  appConfigEndpoint: '${APP_CONFIG_ENDPOINT}',
 };
