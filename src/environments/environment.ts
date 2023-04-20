65715997148ba4e3bfc7ee129a8f7685dbc123e8
mfiloramo <mlfiloramo@gmail.com>
2023-04-20 10:18:11 -0400

    changed env var fetch and file creation in pipeline

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment: any = {
-  production: /*#{PRODUCTION}*/ false,
+  production: /*#{PRODUCTION}*/ '${PRODUCTION}',
   redirectURI: location.origin,
-  apiBaseUrl: /*#{API_BASE_URL}*/ '',
-  azureTenantID: /*#{AZURE_TENANT_URL}*/ '',
-  azureClientID: /*#{AZURE_CLIENT_ID}*/ '',
-  azureAPIClientID: /*#{AZURE_API_CLIENT_ID}*/ '',
-  appConfigEndpoint: /*#{APP_CONFIG_ENDPOINT}*/ '',
+  apiBaseUrl: /*#{API_BASE_URL}*/ '${API_BASE_URL}',
+  azureTenantID: /*#{AZURE_TENANT_URL}*/ '${AZURE_TENANT_URL}',
+  azureClientID: /*#{AZURE_CLIENT_ID}*/ '${AZURE_CLIENT_ID}',
+  azureAPIClientID: /*#{AZURE_API_CLIENT_ID}*/ '${AZURE_API_CLIENT_ID}',
+  appConfigEndpoint: /*#{APP_CONFIG_ENDPOINT}*/ '${APP_CONFIG_ENDPOINT}',
 };
