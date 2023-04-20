6c96906f81ba6d1517a40c93d3a4bd7a23c282a2
mfiloramo <mlfiloramo@gmail.com>
2023-04-20 13:20:37 -0400

    EVR fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,8 @@ export const environment: any = {
   production: '${PRODUCTION}',
   redirectURI: location.origin,
   apiBaseUrl: '${API_BASE_URL}',
-  azureTenantURL: 'DEBUG ENVIRONMENT.TS',
+  azureTenantID: '${AZURE_TENANT_ID}', // THESE VALUES SHOULD NOW GET SET
   azureClientID: '${AZURE_CLIENT_ID}',
   azureAPIClientID: '${AZURE_API_CLIENT_ID}',
-  azureTenantID: 'TESTING',
   appConfigEndpoint: '${APP_CONFIG_ENDPOINT}',
 };
