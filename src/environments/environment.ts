1a2740bf0a3ff614763b7b1a8452243b461ffc4c
mfiloramo <mlfiloramo@gmail.com>
2023-04-20 10:30:28 -0400

    Fixed azure ten id invalid formatting

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment: any = {
   production: /*#{PRODUCTION}*/ '${PRODUCTION}',
   redirectURI: location.origin,
   apiBaseUrl: /*#{API_BASE_URL}*/ '${API_BASE_URL}',
-  azureTenantID: /*#{AZURE_TENANT_URL}*/ '${AZURE_TENANT_URL}',
+  azureTenantURL: `https://login.microsoftonline.com/*#{AZURE_TENANT_ID}*/v2.0`,
   azureClientID: /*#{AZURE_CLIENT_ID}*/ '${AZURE_CLIENT_ID}',
   azureAPIClientID: /*#{AZURE_API_CLIENT_ID}*/ '${AZURE_API_CLIENT_ID}',
   appConfigEndpoint: /*#{APP_CONFIG_ENDPOINT}*/ '${APP_CONFIG_ENDPOINT}',
