88e587477febef09f5c176d61da31b9bda008e89
mfiloramo <mlfiloramo@gmail.com>
2023-04-20 12:54:05 -0400

    Debug EVR

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: '${PRODUCTION}',
   apiBaseUrl: '${API_BASE_URL}',
-  azureTenantURL: '${AZURE_TENANT_URL}',
+  azureTenantURL: 'DEBUG ENVIRONMENT.PROD.TS',
   azureClientID: '${AZURE_CLIENT_ID}',
   azureAPIClientID: '${AZURE_API_CLIENT_ID}',
   appConfigEndpoint: '${APP_CONFIG_ENDPOINT}',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment: any = {
   production: '${PRODUCTION}',
   redirectURI: location.origin,
   apiBaseUrl: '${API_BASE_URL}',
-  azureTenantURL: '${AZURE_TENANT_URL}',
+  azureTenantURL: 'DEBUG ENVIRONMENT.TS',
   azureClientID: '${AZURE_CLIENT_ID}',
   azureAPIClientID: '${AZURE_API_CLIENT_ID}',
   appConfigEndpoint: '${APP_CONFIG_ENDPOINT}',
