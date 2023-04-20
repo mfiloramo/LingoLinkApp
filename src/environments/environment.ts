0ce74d03c687e22d84b8afa9e2f6838913c09086
mfiloramo <mlfiloramo@gmail.com>
2023-04-20 10:39:50 -0400

    Continued environment env var rep tweaks

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment: any = {
-  production: /*#{PRODUCTION}*/ '${PRODUCTION}',
+  production: '#{PRODUCTION}',
   redirectURI: location.origin,
-  apiBaseUrl: /*#{API_BASE_URL}*/ '${API_BASE_URL}',
-  azureTenantURL: `https://login.microsoftonline.com/*#{AZURE_TENANT_ID}*/v2.0`,
-  azureClientID: /*#{AZURE_CLIENT_ID}*/ '${AZURE_CLIENT_ID}',
-  azureAPIClientID: /*#{AZURE_API_CLIENT_ID}*/ '${AZURE_API_CLIENT_ID}',
-  appConfigEndpoint: /*#{APP_CONFIG_ENDPOINT}*/ '${APP_CONFIG_ENDPOINT}',
+  apiBaseUrl: '#{API_BASE_URL}',
+  azureTenantURL: '#{AZURE_TENANT_URL}',
+  azureClientID: '#{AZURE_CLIENT_ID}',
+  azureAPIClientID: '#{AZURE_API_CLIENT_ID}',
+  appConfigEndpoint: '#{APP_CONFIG_ENDPOINT}',
 };
