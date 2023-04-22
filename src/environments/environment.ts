b1c1398aff6742fad7769696646a094da6d41f73
mfiloramo <mlfiloramo@gmail.com>
2023-04-22 12:20:13 -0400

    Fix environment.ts to remove uri

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment: any = {
   production: '${PRODUCTION}',
   redirectURI: '${APP_REDIRECT_URI}',
-  apiBaseUrl: '${API_BASE_URL}/LingoLinkCore/',
+  apiBaseUrl: '${API_BASE_URL}',
   azureTenantID: '${AZURE_TENANT_ID}',
   azureClientID: '${AZURE_CLIENT_ID}',
   azureAPIClientID: '${AZURE_API_CLIENT_ID}',
