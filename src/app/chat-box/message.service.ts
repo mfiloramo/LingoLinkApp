3f526a292982cdebbe3d69415515d7a721c7a244
mfiloramo <mlfiloramo@gmail.com>
2023-05-08 12:36:32 -0500

    Remove environment.local ref

--- src/app/chat-box/message.service.ts
+++ src/app/chat-box/message.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { ChatMessage } from "../../interfaces/message.interfaces";
-import { environment } from '../../environments/environment.local';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
