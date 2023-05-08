65de269eb2375a65d761e81bc69f0a6f0e289218
mfiloramo <mlfiloramo@gmail.com>
2023-05-08 12:21:56 -0500

    Rotate AAD reg keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment: any = {
-  production: '${PRODUCTION}',
+  production: true,
   redirectURI: '${APP_REDIRECT_URI}',
-  apiBaseUrl: '${API_BASE_URL}',
+  apiBaseUrl: 'https://lingolinkapi.azurewebsites.net/api',
   azureTenantID: '${AZURE_TENANT_ID}',
   azureClientID: '${AZURE_CLIENT_ID}',
   azureAPIClientID: '${AZURE_API_CLIENT_ID}',
-  appConfigEndpoint: '${APP_CONFIG_ENDPOINT}',
+  appConfigEndpoint: 'https://lingolink-config.azconfig.io',
 };
