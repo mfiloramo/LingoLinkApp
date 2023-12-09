308e24bd51470352568d533706892b562769d463
Michael Filoramo <72812809+mfiloramo@users.noreply.github.com>
2023-12-09 15:36:35 -0500

    Add debugging to user state distribution

--- src/app/views/settings/settings.view.ts
+++ src/app/views/settings/settings.view.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from "@angular/material/icon";
+import { Language } from "../../../interfaces/Language.interfaces";
+import languageArray from '../../../utils/languageMapper';
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [ CommonModule, MatIconModule ],
+  imports: [ CommonModule, MatIconModule, FormsModule ],
   templateUrl: './settings.view.html',
-  styleUrl: './settings.view.css'
+  styleUrls: ['./settings.view.css', '../../components/message-box/message-box.component.css']
 })
 export class SettingsView {
+  @Output() selectedLanguage: EventEmitter<any> = new EventEmitter<any>();
+  public languageArray: Language[] = languageArray;
 
+  /** PUBLIC METHODS */
+  public selectLanguage(lang: any): void {
+    const selectedLanguage: Language | undefined = this.languageArray.find((language: Language): boolean => language.name === lang.target.value);
+    if (selectedLanguage) {
+      this.selectedLanguage.emit(selectedLanguage);
+      console.log('settings.view.selectedLanguage:', selectedLanguage);
+
+    }
+  }
 }
