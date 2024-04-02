ecbe67bf7f9c99c6b70dedcaf33947e0c18f69a8
Michael Filoramo <72812809+mfiloramo@users.noreply.github.com>
2024-04-02 14:56:57 -0400

    UI changes to Settings

--- src/app/components/contacts/contacts.component.ts
+++ src/app/components/contacts/contacts.component.ts
@@ -17,7 +17,6 @@ import { TitleComponent } from "../title/title.component";
 })
 export class ContactsComponent {
   public stubContacts: string[] = [ 'TestUser_1', 'TestUser_2', 'TestUser_3','Filoramo', 'show_user_1', 'show_user_2', 'show_user_3', 'show_user_4', 'show_user_5', 'show_user_6' ];
-  public contactsMenuButtonStyling: object = { 'background': '#023593', 'color': '#dae3ff' , 'border-bottom': '1px solid #437eec', 'height': '7vh', 'width': '100vw' };
 
   constructor(
     private router: Router,
