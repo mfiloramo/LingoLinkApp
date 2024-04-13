724939e0e4024a711ab0a84792af2212c2a9d8d2
Michael Filoramo <72812809+mfiloramo@users.noreply.github.com>
2024-04-13 13:31:02 -0400

    Prod mode

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -25,7 +25,7 @@ import { MainSettingsView } from "./views/settings/main-settings/main-settings.v
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/home/settings/account', // PROD: '/login'
+    redirectTo: '/login', // PROD: '/login'
     pathMatch: 'full'
   },
   {
