549abc51833e6214ad2b2f5ca94302d9b9881086
Michael Filoramo <72812809+mfiloramo@users.noreply.github.com>
2024-04-17 09:38:48 -0400

    Add chevrons to main-settings nav buttons

--- src/app/views/settings/settings.view.ts
+++ src/app/views/settings/settings.view.ts
@@ -5,12 +5,15 @@ import { MatIconModule } from "@angular/material/icon";
 import { LanguageSelectorComponent } from "../../components/language-selector/language-selector.component";
 import { RouterLink, RouterOutlet } from "@angular/router";
 import { TitleComponent } from "../../components/title/title.component";
+import { routerAnimationFade } from "../../../utils/routerAnimations";
 
 @Component({
   selector: 'app-settings',
   standalone: true,
   imports: [ CommonModule, MatIconModule, FormsModule, LanguageSelectorComponent, RouterLink, RouterOutlet, TitleComponent ],
-  templateUrl: './settings.view.html'
+  templateUrl: './settings.view.html',
+  animations: routerAnimationFade,
+
 })
 export class SettingsView {
   // PUBLIC METHODS
