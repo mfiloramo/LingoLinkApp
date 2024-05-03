d6654424b8ac5404d7da67194066e37013540393
Michael Filoramo <72812809+mfiloramo@users.noreply.github.com>
2024-05-02 23:11:40 -0400

    Build initial text inputs for change-user-data views

--- src/app/views/settings/account/change-email/change-email.view.ts
+++ src/app/views/settings/account/change-email/change-email.view.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserService } from "../../../../services/user/user.service";
 
 @Component({
   selector: 'app-change-email',
   standalone: true,
-  imports: [CommonModule],
+  imports: [ CommonModule ],
   templateUrl: './change-email.view.html',
 })
 export class ChangeEmailView {
+  public currentEmail!: string;
 
+  constructor(public userService: UserService) {
+    this.currentEmail = this.userService.userState().email;
+  }
 }
--- src/app/views/settings/account/change-username/change-username.view.ts
+++ src/app/views/settings/account/change-username/change-username.view.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserService } from "../../../../services/user/user.service";
 
 @Component({
   selector: 'app-change-username',
   standalone: true,
-  imports: [CommonModule],
+  imports: [ CommonModule ],
   templateUrl: './change-username.view.html',
 })
 export class ChangeUsernameView {
+  public currentUsername!: string;
 
+  constructor(public userService: UserService) {
+    this.currentUsername = this.userService.userState().username;
+  }
 }
